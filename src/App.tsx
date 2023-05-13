70ca2239e6e3db61a9b1eb8b2c36bf3c425cef6d
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-05-13 13:33:15 -0400

    Adding more pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar, Home } from './components';
 import Footer from './components/Footer';
 import Dashboard from './components/Dashboard';
+import RideRequest from './components/RideRequest';
+import RideSearch from './components/RideSearch';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='/ride-request' element={<RideRequest/>} />
+        <Route path='/ride-search' element={<RideSearch/>} />
       </Routes>
       <Footer />
     </Router>
