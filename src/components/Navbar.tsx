df7e472a6fcb591074137334f0cee88b71330c88
Dissident <kak100kak@gmail.com>
2023-05-12 22:00:28 -0400

    Set up app architecture and add pages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+/**
+ * Navigation bar component.
+ */
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <ul>
+            <li>
+              <div>
+                <NavLink to="/">Home</NavLink>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
