ca2d386db499e96b4303afeba3dd1efb00da82ea
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2023-05-13 16:00:37 -0400

    Creating a popup and buttons for when a ride is requested

--- src/components/RideSearch.tsx
+++ src/components/RideSearch.tsx
@@ -2,11 +2,25 @@ import { drivers } from './data/drivers'
 
 const RideSearch = () => {
 
-    const requestRide = (driverid?:string | null) => {
+    const closePopup = () => {
+        document.getElementById('request-popup').style.display='none';
+    }
+
+    const requestRide = (driverid?:string, drivername?:string, driverdistance?:string) => {
         console.log(driverid)
         if (driverid !== null){
             document.getElementById(driverid+'-btn').innerHTML = 'Waiting for Response...';
         }
+
+        let popup = document.createElement("div"); popup.classList.add('request-popup'); popup.id = 'request-popup'
+            let message_title = document.createElement('h3'); message_title.innerHTML = 'Hi there!';
+            let message = document.createElement('p');
+            message.innerHTML = 'You have sent a carpool request to ' + drivername + ' . They are currently ' + driverdistance + ' from your current location. We will notify you if they accept or decline your request. Safe travels!'
+            let close_btn = document.createElement('button'); close_btn.id='close-btn'; close_btn.classList.add('action-btn'); close_btn.innerHTML = 'ok'; close_btn.onclick=closePopup
+            popup.appendChild(message_title);
+            popup.appendChild(message);
+            popup.appendChild(close_btn);
+        document.getElementById('ride-request-page').appendChild(popup);
     }
 
     return(
@@ -23,7 +37,7 @@ const RideSearch = () => {
                             <h5>This driver is {data.distance} away from you.</h5>
                         </div>
                         <div className='section_2'>
-                            <button onClick={() => requestRide(data.driver_id)} id={data.driver_id + '-btn'}>Request a Ride</button>
+                            <button onClick={() => requestRide(data.driver_id, data.driver_name, data.distance)} id={data.driver_id + '-btn'}>Request a Ride</button>
                         </div>
                     </div>
                 )
