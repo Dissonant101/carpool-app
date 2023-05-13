c4a21de38f198ed7484afc226ff119f76ad0641f
Sean Wang <seanwaterloo2997@gmail.com>
2023-05-13 11:22:57 -0400

    Update Dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,18 @@
+const Dashboard = () => {
+    return (
+        <div>
+            <div className="dashboard-container">
+                <div>
+                    <img src="Drive.gif" alt="Drive" />
+                    <h2>Drive</h2>
+                </div>
+                <div>
+                    <img src="Ride.png" alt="Ride" />
+                    <h2>Ride</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard
