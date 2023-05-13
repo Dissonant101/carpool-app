9723a8ac7e9697fb1858fd1d4af49a2d78f3d8b9
Dissident <kak100kak@gmail.com>
2023-05-12 22:06:45 -0400

    Add prettier settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Navbar, Home } from "./components";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navbar, Home } from './components';
 
 function App() {
   return (
